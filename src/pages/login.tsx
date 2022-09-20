dc146fe0c8d1ef9e78392186c359af4c74f26c8b
Seungwoo Lee <timosean@naver.com>
2022-09-20 23:37:29 +0900

    fix: tailwind.config.js content paths

---
+++ src/pages/login.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+  return (
+    <main className="w-[100vw] h-[100vh] flex">
+      <section className="w-2/12 h-full flex justify-center align-center bg-[#D7C9B2]">
+        <img src="/emblem.svg" className="w-full" />
+      </section>
+      <section className="w-10/12 h-full flex">gd</section>
+    </main>
+  );
+};
+
+export default LoginPage;
